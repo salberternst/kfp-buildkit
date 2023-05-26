3753b7b6a432c28d34806a5699887536678dab21
salberternst <31729433+salberternst@users.noreply.github.com>
2023-05-26 14:54:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='kfp-buildkit',
+setup(name='docker',
       version='1.0',
       description='Small docker stub for the kfp client to build images using (rootless) buildkit',
       author='Sebastian Alberternst',
       author_email='alberternst@gmail.com',
-      url='https://www.python.org/sigs/distutils-sig/',
+      url='https://github.com/salberternst/kfp-buildkit',
       packages=['docker'],
       )
